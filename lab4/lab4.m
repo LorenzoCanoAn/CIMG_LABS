2720e552ab498a40640786314f2825c37873811c
Lorenzo Cano <lcano@unizar.es>
2021-03-29 19:55:51 +0200

    empezado local tone mapping

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -6,22 +6,25 @@ loadManager = imgScanner(inputFolder);
 
 %% HDR IMAGING
     %% Lienarize images
-    [~, nImages] = size(loadManager.img);
-    [g] = get_cameraResponse(loadManager.img, nImages, loadManager.obt, 20, 20);
+%     [~, nImages] = size(loadManager.img);
+%     [g] = get_cameraResponse(loadManager.img, nImages, loadManager.obt, 20, 20);
 
-    
     %% Obtain radiance map
-    radianceMap = hdrRadiance(loadManager, g);
-    figure;
-    imshow(radianceMap)
-    title("Radiance map");
-    
+%     radianceMap = hdrRadiance(loadManager, g);
+%     figure;
+%     imshow(radianceMap)
+%     title("Radiance map");
+
 %% GLOBAL TONE MAPPING
-    im1 = global_tone_mapping(radianceMap,0.00001,0.18,2);
-    figure;
-    imshow(im1);
-    title("Global tone mapping");
-    
+%     globalTone = global_tone_mapping(radianceMap,0.00001,0.18,2);
+%     figure;
+%     imshow(globalTone);
+%     title("Global tone mapping");
+
 %% LOCAL TONE MAPPING
+    localTone = local_tone_mapping(radianceMap,0.00002);
+    figure;
+    imshow(localTone);
+    title("Local tone mapping");
     
 %% Trial with own pictures
