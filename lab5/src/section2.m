6658306163d06200466c8588cdaed0c75ed4d317
Lorenzo Cano <736078@unizar.es>
2021-04-27 15:53:34 +0200

    Lmin y Lmax de seccion 2 calculadas

--- lab5/src/section2.m
+++ lab5/src/section2.m
@@ -1,23 +1,32 @@
 data_folder = "../data/Section2_SingleCapture";
 files = dir(data_folder);
 files = files(3:end);
+global params
+params.kernel = ones(1,10)/10;
+params.c1 = 0.8;
+params.c2 = 0.1;
+params.c3 = 0.1;
 
 for file = files'
     image = imread(fullfile(file.folder,file.name));
+    % Get Lmin and Lmax
     [Lmin,Lmax] = get_Lmin_Lmax(image);
+    % Interpolate Lmin and Lmax
     
 end
 
 
 
 function [Lmin, Lmax] = get_Lmin_Lmax(image)
-    Lmin = get_Lmin(image);
-    Lmax = get_Lmax(image);
-end
-function Lmin = get_Lmin(image)
-    
-end
 
-function Lmax = get_Lmax(image)
+global params
+hsv_image = rgb2hsv(image);
+
+convImage = conv2(hsv_image(:,:,3),params.kernel,'same');
+i_max = hsv_image(:,:,3) > convImage;
+i_min = ~i_max;
+
+Lmin = image.*uint8(i_min);
+Lmax = image.*uint8(i_max);
 
 end
